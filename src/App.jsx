386ad053a73c5598943c879293e8a5903ac05a8c
Facundo Zuleta <facundozuleta70@gmail.com>
2024-09-12 14:32:02 -0300

    logo header

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import { Home } from './components/Home'
 import { Login } from './components/Login'
 import { Register } from './components/Register'
 import { Perfil } from './components/Perfil'
+import Header from './layout/Header'
  
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/login' element={<Login />} />
